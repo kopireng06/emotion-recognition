1b7364f33d860bb9fc4def777adadd3cd6122b15
Helmi Agil Achmani <helmi.agilachmani@advotics.com>
2022-09-04 02:00:16 +0700

    add evaluation and try export model to json

--- cnn.py
+++ cnn.py
@@ -2,7 +2,9 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.preprocessing import image
+from confusion_matrix import plot_confusion_matrix
 import matplotlib.pyplot as plt
+import numpy as np
 
 train_images = keras.preprocessing.image_dataset_from_directory(
     './FER2013/train', image_size=(48, 48), color_mode='grayscale')
@@ -49,5 +51,36 @@ epochs = 60
 
 history4 = model.fit(train_set, validation_data=test_set, epochs=epochs)
 
+y_pred = model.predict(train_set)
+print("y_pred")
+y_pred = np.argmax(y_pred, axis=1)
+class_labels = train_set.class_indices
+class_labels = {v:k for k,v in class_labels.items()}
+
+from sklearn.metrics import classification_report, confusion_matrix
+cm_train = confusion_matrix(train_set.classes, y_pred)
+print('Classification Report')
+target_names = list(class_labels.values())
+print(classification_report(train_set.classes, y_pred, target_names=target_names))
+plot_confusion_matrix(cm_train, target_names)
+
+y_pred = model.predict(test_set)
+print("y_pred")
+y_pred = np.argmax(y_pred, axis=1)
+class_labels = test_set.class_indices
+class_labels = {v:k for k,v in class_labels.items()}
+
+from sklearn.metrics import classification_report, confusion_matrix
+cm_test = confusion_matrix(test_set.classes, y_pred)
+print('Classification Report')
+target_names = list(class_labels.values())
+print(classification_report(test_set.classes, y_pred, target_names=target_names))
+plot_confusion_matrix(cm_test, target_names)
+
+import tensorflowjs as tfjs
+tfjs.converters.save_keras_model(model, './')
+
+
 # https://www.kaggle.com/code/songdevelop/facial-emotion-recogination
 # https://www.kaggle.com/code/amankumar2004/emotion-detection-recognition
+# https://towardsdatascience.com/deploying-an-image-classifier-using-javascript-84da1480b3a4
